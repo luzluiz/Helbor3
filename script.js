4f1e1e5102625a81cd0eda6943de9287190d20c2
Luiz Felipe <luizluzsf@gmail.com>
2025-01-13 23:55:40 -0300

    alteração footer, adiçao de novas salas, adição de botao carregar em salas, adicao de funcao nos cards e novas fotos

--- script.js
+++ script.js
@@ -48,3 +48,78 @@ document.addEventListener("DOMContentLoaded", function() {
     locationSection.classList.add('visible');
 });
 
+
+const serviceData = [
+    {
+        title: 'Estacionamento Estapar',
+        description: 'Entrada pelo subsolo à esquerda.',
+        hours: '',
+        images: ['img/estacionamento1.JPG', 'img/estacionamento2.JPG', 'img/estaparplaca.jpg'],
+    },
+    {
+        title: 'Trio Cafeteria',
+        description: 'Cafés especiais e refeições rápidas.',
+        hours: 'Segunda a Sexta: 07:00 - 19:00',
+        images: ['img/triocafe.JPG', 'img/cafe1.jpg'],
+    },
+    {
+        title: 'Salas de Reunião',
+        description: 'Espaços modernos para reuniões e eventos locado apenas para condôminos.',
+        hours: 'Sob agendamento.',
+        images: ['img/auditorios.jpg', 'img/aud2.avif', 'img/aud3.avif'],
+    },
+];
+
+let currentService = 0;
+let currentImage = 0;
+
+function openModal(serviceIndex) {
+    currentService = serviceIndex;
+    currentImage = 0;
+    const service = serviceData[serviceIndex];
+    document.getElementById('modal-title').textContent = service.title;
+    document.getElementById('modal-description').textContent = service.description;
+    document.getElementById('modal-hours').textContent = service.hours;
+    document.getElementById('gallery-image').src = service.images[currentImage];
+    document.getElementById('service-modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('service-modal').style.display = 'none';
+}
+
+function changeImage(direction) {
+    const images = serviceData[currentService].images;
+    currentImage = (currentImage + direction + images.length) % images.length;
+    document.getElementById('gallery-image').src = images[currentImage];
+}
+
+let currentItemCount = 10; // Número de itens inicialmente visíveis
+const items = document.querySelectorAll('#room-list li');
+const loadMoreButton = document.getElementById('load-more-btn');
+
+function showItems(count) {
+    for (let i = 0; i < items.length; i++) {
+        if (i < count) {
+            items[i].style.display = 'list-item';
+        } else {
+            items[i].style.display = 'none';
+        }
+    }
+}
+
+function loadMoreItems() {
+    currentItemCount += 20; // Aumenta o número de itens visíveis por vez
+    showItems(currentItemCount);
+
+    // Se o número de itens visíveis for igual ou maior que o total, esconde o botão
+    if (currentItemCount >= items.length) {
+        loadMoreButton.style.display = 'none';
+    }
+}
+
+// Inicializa a lista com os itens visíveis
+showItems(currentItemCount);
+
+// Adiciona o evento de clique no botão
+loadMoreButton.addEventListener('click', loadMoreItems);
